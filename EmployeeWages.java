f203762058615a717746ef5908a31c97470d35df
prashant169 <prashantphad2018@gmail.com>
2021-09-25 04:31:39 -0700

    Employee Wage Using Seitch Case

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -96,6 +96,53 @@ public static void CalculateDailyWages()
  	{
 		System.out.println("Employee Absent.");
 	}
-}
+ }
 
-} 
+ }  
+
+
+/* Uc4 Employee Wage Using Switch Case
+
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class EmployeeWages{
+        public static void main( String []args )
+        {
+                System.out.println("Welcome");
+                Jadeja();
+        }
+        static void Jadeja(){
+                Scanner in = new Scanner(System.in);
+                int PartTimeHour = 4;
+                int RatePerHour = 20;
+                int FullDayHour = 8;
+                int Day = 0 ;
+                Random rn = new Random ();
+                int rand =rn.nextInt(3);
+                        switch(rand){
+                                        case 1:
+                                                System.out.println("IT's Full Time Wages: ");
+                                                System.out.println("Enter NUmber Of Working Day's: ");
+                                                Day = in.nextInt();
+                                                int FullTimeWages = RatePerHour * (FullDayHour * Day);
+                                                System.out.println("FullTimeWages for " + Day +" Days: " + "Rs." + FullTimeWages);
+                                                break;
+                       
+                                        case 2:
+                                                System.out.println("IT's Part Time Wages: ");
+                                                System.out.println("Enter Number Of Working Day's: ");
+                                                Day = in.nextInt();
+                                                int PartTimeWages = Day * PartTimeHour * FullDayHour;
+                                                System.out.println("PartTimeWages for " + Day +" days: " + "Rs." + PartTimeWages);
+                                                break;
+                                        default:
+                                                System.out.println("Employee is Absent..........!");
+                                                break;
+                                     }
+                        in.close();
+        		}
+        }
