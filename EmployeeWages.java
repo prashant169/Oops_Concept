843984d26e42ca6653e0e76e8e3ceec81fbf0a6e
prashant169 <prashantphad2018@gmail.com>
2021-09-25 04:26:32 -0700

    CalculatePartTimeWage

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -61,3 +61,41 @@ public static void CalculateDailyWages()
 }
 
 } 
+
+
+
+
+/* uc3 Calculate Part Time Wage
+
+
+import java.util.Random;
+
+public class EmployeeWages {
+	public static void main( String args[] ){
+		System.out.println("Welcome To The Employee Wages");
+		CalculateDailyWages();
+}
+
+public static void CalculateDailyWages()
+{
+	int IsFullTime = 1;
+	Random rn = new Random();
+	int check= rn.nextInt(2);
+	int RatePerHour = 20;
+	int PartTimeHour = 4;
+	int FullDayHour = 8;
+	int TotalWages= 0;
+	if(check == IsFullTime)
+	{
+		System.out.println("Employee Present.");
+		TotalWages = RatePerHour * PartTimeHour;
+		System.out.println("Daily PartTime Wages  : "+ TotalWages);
+		
+	}
+	else
+ 	{
+		System.out.println("Employee Absent.");
+	}
+}
+
+} 
